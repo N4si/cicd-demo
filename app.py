fc69bd911834790d480106f0352f653ba406a269
meenausa <157774162+meenausa@users.noreply.github.com>
2024-03-07 07:44:02 +0530

    Updated the code

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from urllib.parse import quote  # Import quote from urllib.parse
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Hello CI CD World!"
+    # Use the quote function from urllib.parse
+    return quote("Hello CI CD World!")
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
